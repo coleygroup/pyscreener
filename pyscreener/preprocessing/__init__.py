dcad5cc9457ccc6cfa8a477d4d0d9efebd029018
david graff <deg711@g.harvard.edu>
2020-07-24 13:43:31 -0400

    forgot something

---
+++ pyscreener/preprocessing/__init__.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def preprocess(preprocessing_options: List[str], **kwargs):
+    if 'pdbfix' in preprocessing_options:
+        pass
+    if 'autobox' in preprocessing_options:
+        pass
+    if 'tautomer' in preprocessing_options:
+        pass
+    if 'desalt' in preprocessing_options:
+        pass
+    if 'filter' in preprocessing_options:
+        pass

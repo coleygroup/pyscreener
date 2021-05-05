49562242fd4e9f1480d1714cf2b1b7236afefc7a
david graff <deg711@g.harvard.edu>
2021-05-05 16:28:19 -0400

    moving around functions in base.py

--- pyscreener/docking/base.py
+++ pyscreener/docking/base.py
@@ -181,6 +181,51 @@ class Screener(ABC):
             path.mkdir(parents=True)
         self.__tmp_out = path
 
+    def collect_files(self, out_path: Optional[str]=None):
+        """Collect all the files from the local disks of the respective nodes
+
+        For I/O purposes, input and output files for each simulation are 
+        created on the local disk of each node. If these files are desired at 
+        the end, they must be copied over from the node's local file system to 
+        the final destination.
+        
+        This is achieved by creating a gzipped tar file of the temp directory 
+        (the one that contains all of the input and output files for 
+        simulations conducted on that node) and moving these tar files under 
+        the desired path. Each tar file is named according the node ID from 
+        which it originates.
+
+        This function should ideally only be called once during the lifetime
+        of a Screener because it is slow and early calls will yield nothing
+        over a single, final call.
+
+        Parameters
+        ----------
+        out_path : Optional[str], defualt=None
+            the path under which the tar files should be collected to. If None,
+            use self.path
+        """
+        out_path = out_path or self.path
+        out_path = Path(out_path)
+        if not out_path.is_dir():
+            out_path.mkdir(parents=True)
+
+        refs = []
+        for node in ray.nodes():    # run on all nodes
+            address = node["NodeManagerAddress"]
+            @ray.remote(resources={f'node:{address}': 0.1})
+            def zip_and_move_tmp():
+                output_id = re.sub('[:,.]', '', ray.state.current_node_id())
+                tmp_tar = (self.tmp_dir / output_id).with_suffix('.tar.gz')
+
+                with tarfile.open(tmp_tar, 'w:gz') as tar:
+                    tar.add(self.tmp_dir, arcname=output_id)
+
+                shutil.move(str(tmp_tar), str(out_path))
+
+            refs.append(zip_and_move_tmp.remote())
+        ray.wait(refs)
+        
     @property
     def receptors(self) -> List[str]:
         return self.__receptors
@@ -398,51 +443,6 @@ class Screener(ABC):
                   f'({total/len(recordsss):0.3f} s/ligand)', flush=True)
 
         return recordsss
-
-    def collect_files(self, out_path: Optional[str]=None):
-        """Collect all the files from the local disks of the respective nodes
-
-        For I/O purposes, input and output files for each simulation are 
-        created on the local disk of each node. If these files are desired at 
-        the end, they must be copied over from the node's local file system to 
-        the final destination.
-        
-        This is achieved by creating a gzipped tar file of the temp directory 
-        (the one that contains all of the input and output files for 
-        simulations conducted on that node) and moving these tar files under 
-        the desired path. Each tar file is named according the node ID from 
-        which it originates.
-
-        This function should ideally only be called once during the lifetime
-        of a Screener because it is slow and early calls will yield nothing
-        over a single, final call.
-
-        Parameters
-        ----------
-        out_path : Optional[str], defualt=None
-            the path under which the tar files should be collected to. If None,
-            use self.path
-        """
-        out_path = out_path or self.path
-        out_path = Path(out_path)
-        if not out_path.is_dir():
-            out_path.mkdir(parents=True)
-
-        refs = []
-        for node in ray.nodes():    # run on all nodes
-            address = node["NodeManagerAddress"]
-            @ray.remote(resources={f'node:{address}': 0.1})
-            def zip_and_move_tmp():
-                output_id = re.sub('[:,.]', '', ray.state.current_node_id())
-                tmp_tar = (self.tmp_dir / output_id).with_suffix('.tar.gz')
-
-                with tarfile.open(tmp_tar, 'w:gz') as tar:
-                    tar.add(self.tmp_dir, arcname=output_id)
-
-                shutil.move(str(tmp_tar), str(out_path))
-
-            refs.append(zip_and_move_tmp.remote())
-        ray.wait(refs)
 
     def get_smis(self, source: Union[str, Sequence[str]],
                  **kwargs) -> Tuple[List[str], List[str]]:

abefc24ec40f72786a83391223a536a9d9ed9a60
david graff <deg711@g.harvard.edu>
2021-07-27 12:03:16 -0400

    adding documentation to help command

--- scripts/get_files.py
+++ scripts/get_files.py
@@ -6,13 +6,22 @@ import tarfile
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--smis', nargs='+')
-    parser.add_argument('-d', '--root-dir', required=True)
-    parser.add_argument('-p', '--path')
+    parser.add_argument('-s', '--smis', nargs='+',
+                        help='the individual SMILES strings you would like to extract files for. NOTE: the SMILES strings must be quoted to avoid command line parsing issues.')
+    parser.add_argument('-f', '--files', nargs='+',
+                        help='a file (or files) containing the desired SMILES strings, each on a separate line. NOTE: there must be no title line!')
+    parser.add_argument('-d', '--root-dir', required=True,
+                        help='the output directory of a pyscreener run')
+    parser.add_argument('-p', '--path',
+                        help='the desired output directory for the extracted input and output file. By default, use the pyscreener output directory.')
 
     args = parser.parse_args()
     smis = set(args.smis)
     
+    for f in args.files:
+        with open(f) as fid:
+            smis.update(fid.read().splitlines())
+
     root_dir = Path(args.root_dir)
     if not root_dir.is_dir():
         raise ValueError(f'"{args.root_dir}" is not a directory!')

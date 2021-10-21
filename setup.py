b4a93e3b849eef872f9102441c8d972b9be49cff
david graff <deg711@g.harvard.edu>
2021-10-21 15:19:52 -0400

    cleaning up setup.py

--- setup.py
+++ setup.py
@@ -16,49 +16,43 @@ except:
 
 
 setup(
-    name='pyscreener',
-    author='david graff',
-    author_email='deg711@g.harvard.edu',
+    name="pyscreener",
+    author="david graff",
+    author_email="deg711@g.harvard.edu",
     description=short_description[0],
     long_description=long_description,
     long_description_content_type="text/markdown",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    license='MIT',
-
-    # Which Python importable modules should be included when your package is 
-    # installed. Handled automatically by setuptools. Use 'exclude' to prevent 
-    # some specific subpackage(s) from being added, if needed
-    packages=find_packages(
-        exclude=[
-            'pyscreener.dft',
-            'pyscreener.md'
-        ]
-    ),
-
-    # Optional include package data to ship with your package
-    # Customize MANIFEST.in if the general case does not suit your needs
-    # Comment out this line to prevent the files from being packaged with your software
+    license="MIT",
+    packages=find_packages(exclude=["pyscreener.dft", "pyscreener.md", "pyscreener.docking.dock.scripts"]),
     include_package_data=True,
     setup_requires=[],
-
-    # url='http://www.my_package.com',  # Website
-    platforms=['Linux', 'Mac OS-X', 'Unix'],
+    url="https://github.com/coleygroup/pyscreener",
+    platforms=["Linux", "Mac OS-X", "Unix"],
     python_requires=">=3.7",
     install_requires=[
-        'configargparse',
-        'h5py',
-        'numpy',
-        'ray[default]',
-        'pandas',
-        'pdbfixer @ git+https://github.com/openmm/pdbfixer.git',
-        'seaborn',
-        'scikit_learn',
-        'scipy',
-        'tqdm'
+        "configargparse",
+        "h5py",
+        "numpy",
+        "ray[default]",
+        "pandas",
+        "pdbfixer @ git+https://github.com/openmm/pdbfixer.git",
+        "seaborn",
+        "scikit_learn",
+        "scipy",
+        "tqdm",
     ],
-
-    # Manual control if final package is compressible or not, set False to prevent the .egg from being made
-    # zip_safe=False,
-
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Pytest",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Typing :: Typed"
+    ]
 )

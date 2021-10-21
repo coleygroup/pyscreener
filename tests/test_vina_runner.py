adf8675c620df030cd7664196e3fa233c9249a8a
david graff <deg711@g.harvard.edu>
2021-10-21 10:09:14 -0400

    another attempt at fixing ci

--- tests/test_vina_runner.py
+++ tests/test_vina_runner.py
@@ -1,71 +1,95 @@
 from pathlib import Path
-import random
-import uuid
 
 import pytest
 
-from pyscreener.docking import CalculationData, vina
-from pyscreener.exceptions import NotSimulatedError
+from pyscreener.exceptions import MissingExecutableError, NotSimulatedError
 from pyscreener.utils import calc_score
-from pyscreener.utils.utils import ScoreMode
+
+try:
+    from pyscreener.docking import CalculationData, vina
+except MissingExecutableError:
+    pytestmark = pytest.mark.skip()
 
 TEST_DIR = Path(__file__).parent
+RECEPTOR_FILEPATH = TEST_DIR / "5WIU.pdb"
 
-RECEPTOR_FILEPATH = TEST_DIR / '5WIU.pdb'
 
-@pytest.fixture(
-    params=["CCCC", "c1ccccc1", "CC(=O)CC", "CN=C=O", "CC(=O)C"]
-)
+@pytest.fixture(params=["CCCC", "c1ccccc1", "CC(=O)CC", "CN=C=O", "CC(=O)C"])
 def smi(request):
     return request.param
 
+
 @pytest.fixture
 def receptor():
-    return TEST_DIR / '5WIU.pdb'
+    return TEST_DIR / "5WIU.pdb"
+
 
 @pytest.fixture
 def center():
     return (-18.2, 14.4, -16.1)
 
+
 @pytest.fixture
 def size():
     return (15.4, 13.9, 14.5)
 
+
 @pytest.fixture
 def in_path(tmp_path):
-    p = tmp_path / 'inputs'
+    p = tmp_path / "inputs"
     p.mkdir(parents=True, exist_ok=True)
 
     return p
+
 
 @pytest.fixture
 def out_path(tmp_path):
-    p = tmp_path / 'outputs'
+    p = tmp_path / "outputs"
     p.mkdir(parents=True, exist_ok=True)
 
     return p
+
 
 @pytest.fixture
 def data(smi, receptor, center, size, in_path, out_path):
     return CalculationData(
-        smi, receptor, center, size, vina.VinaMetadata(),
-        -1, 'ligand', None, in_path, out_path
+        smi,
+        receptor,
+        center,
+        size,
+        vina.VinaMetadata(),
+        -1,
+        "ligand",
+        None,
+        in_path,
+        out_path,
     )
+
 
 def test_prepare_ligand(data):
     data = vina.VinaRunner.prepare_ligand(data)
 
     assert data.metadata.prepared_ligand.exists()
 
+
 def test_prepare(receptor, center, size, in_path, out_path):
     data = CalculationData(
-        "c1ccccc1", receptor, center, size, vina.VinaMetadata(),
-        4, 'ligand', None, in_path, out_path
+        "c1ccccc1",
+        receptor,
+        center,
+        size,
+        vina.VinaMetadata(),
+        4,
+        "ligand",
+        None,
+        in_path,
+        out_path,
     )
     data = vina.VinaRunner.prepare(data)
 
     assert data.metadata.prepared_receptor.exists()
     assert data.metadata.prepared_ligand.exists()
+
 
 def test_run(data):
     vina.VinaRunner.prepare(data)

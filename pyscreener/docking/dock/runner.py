ca2de6289fcc7eed397be272373eeca51d651f15
david graff <deg711@g.harvard.edu>
2022-12-14 12:50:37 -0500

    fix the `dock` runner too

--- pyscreener/docking/dock/runner.py
+++ pyscreener/docking/dock/runner.py
@@ -229,7 +229,8 @@ class DOCKRunner(DockingRunner):
         scores = DOCKRunner.parse_logfile(logfile)
         score = None if scores is None else reduce_scores(scores, sim.reduction, k=sim.k)
 
-        sim.result = Result(sim.smi, name, re.sub("[:,.]", "", ray.state.current_node_id()), score)
+        node_id = re.sub("[:,.]", "", ray.util.get_node_ip_address())
+        sim.result = Result(sim.smi, name, node_id, score)
 
         return scores
 
